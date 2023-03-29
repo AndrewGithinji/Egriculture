703b1ad92c1fe8fabd5e3f8d34e24a735bf6b6ec
AndrewGithinji <andrewgithinji@hotmail.com>
2023-03-29 20:50:48 +0300

    components & JSON files

---
+++ src/app/amount-selector/amount-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amount-selector',
+  templateUrl: './amount-selector.component.html',
+  styleUrls: ['./amount-selector.component.css']
+})
+export class AmountSelectorComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CartComponent } from './cart/cart.component';
+import { CartProductComponent } from './cart-product/cart-product.component';
+import { ButtonComponent } from './button/button.component';
+import { AmountSelectorComponent } from './amount-selector/amount-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductItemComponent,
+    ProductListComponent,
+    ProductItemDetailComponent,
+    PageNotFoundComponent,
+    CheckoutFormComponent,
+    ConfirmationComponent,
+    HeaderComponent,
+    DashboardComponent,
+    CartComponent,
+    CartProductComponent,
+    ButtonComponent,
+    AmountSelectorComponent
   ],
   imports: [
     BrowserModule,
